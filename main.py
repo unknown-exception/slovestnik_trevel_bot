b712ae92d093aee441b09853b7e8cd9d5b67f6af
unknown-exception <yuri.kropochev@outlook.com>
2023-09-25 17:37:34 +0200

    fix bug getting session fix tours headers

--- main.py
+++ main.py
@@ -13,24 +13,31 @@ kb = [
     [
         types.KeyboardButton(text="Aвтобусные туры"),
         types.KeyboardButton(text="Vylety со Словестником"),
-        types.KeyboardButton(text="Походы")
+        types.KeyboardButton(text="Походы"),
     ],
 ]
 MAIN_KEYBOARD = types.ReplyKeyboardMarkup(keyboard=kb)
 
 print("hello world")
 
+
 def print_tours(trs: [tours.Tour]):
     urlkb = InlineKeyboardMarkup(row_width=2)
     for t in trs:
-        urlButton1 = InlineKeyboardButton(text=t.head, callback_data="order_tour_"+t.id)
-        urlButton2 = InlineKeyboardButton(text='Подробнее', url=t.url)
-        urlkb.add(urlButton1,urlButton2 )
+        urlButton1 = InlineKeyboardButton(
+            text=t.head, callback_data="order_tour_" + t.id
+        )
+        urlButton2 = InlineKeyboardButton(text="Подробнее", url=t.url)
+        urlkb.add(urlButton1, urlButton2)
     return urlkb
+
 
 @dp.message_handler(content_types=["new_chat_members"])
 async def new_member(message: types.Message):
-    await message.reply("Приветствуем, на какие мероприятия вы хотите записаться?", reply_markup=MAIN_KEYBOARD)
+    await message.reply(
+        "Приветствуем, на какие мероприятия вы хотите записаться?",
+        reply_markup=MAIN_KEYBOARD,
+    )
 
 
 @dp.callback_query_handler()
@@ -39,9 +46,12 @@ async def check_button(call: types.CallbackQuery):
     user_id = call.from_user.id
     tour = call.data.split("_")[2]
     tmp_order[user_id] = tour
-    await call.message.answer(f"""Вы хотите записаться на [{tours.get_tour_by_id(tour).head}]
-пожалуйста напишите ФИО, количество человек/из них детей, контактная информация.""")
+    await call.message.answer(
+        f"""Вы хотите записаться на [{tours.get_tour_by_id(tour).head}]
+пожалуйста напишите ФИО, количество человек/из них детей, контактная информация."""
+    )
     await call.answer()
+
 
 @dp.message_handler(commands=["order"])
 async def order_command(message: types.Message):
@@ -53,7 +63,7 @@ async def order_command(message: types.Message):
     
     Тебе приходит сообщение/письмо 'пользователь {message.from_user.id} {message.from_user.url} {message.from_user.username} записался на {tour_name}, [{message.text.replace("/order","")}]
     """
-    
+
     await message.reply(reply, reply_markup=MAIN_KEYBOARD)
 
 
@@ -74,28 +84,46 @@ async def send_welcome(message: types.Message):
     await message.reply(
         """Привет! Я бот Поездок со Словестником!
 Нажмите кнопку для выбора экскурсий""",
-         reply_markup=MAIN_KEYBOARD
+        reply_markup=MAIN_KEYBOARD,
     )
+
 
 @dp.message_handler(regexp="Походы")
 async def hiking_list(message: types.Message):
-    markup = print_tours( tours.get_bus())
-    await message.answer(f"""Расписание по запросу {message.text}
-Левая кнопка - записаться на поездку, правая - посмотреть подробную информацию"""
-                         , reply_markup=markup)
+    markup = print_tours(tours.get_hike())
+    await message.answer(
+        f"""Расписание по запросу {message.text}
+Левая кнопка - записаться на поездку, правая - посмотреть подробную информацию""",
+        reply_markup=markup,
+    )
+
 
 @dp.message_handler(regexp="Vylety со Словестником")
 async def tours_list(message: types.Message):
-    markup = print_tours( tours.get_tours())
-    await message.answer(f"""Расписание по запросу {message.text}
-Левая кнопка - записаться на поездку, правая - посмотреть подробную информацию"""
-                         , reply_markup=markup)
+    markup = print_tours(tours.get_tours())
+    await message.answer(
+        f"""Расписание по запросу {message.text}
+Левая кнопка - записаться на поездку, правая - посмотреть подробную информацию""",
+        reply_markup=markup,
+    )
+
+
+@dp.message_handler(regexp="Aвтобусные туры")
+async def bus_list(message: types.Message):
+    markup = print_tours(tours.get_bus())
+    await message.answer(
+        f"""Расписание по запросу {message.text}
+Левая кнопка - записаться на поездку, правая - посмотреть подробную информацию""",
+        reply_markup=markup,
+    )
 
 
 @dp.message_handler()
 async def echo123(message: types.Message):
-    if not tmp_order[message.chat.id]:
-        await message.answer("Я не знаю такой команды, извините", reply_markup=MAIN_KEYBOARD)
+    if not message.chat.id in tmp_order:
+        await message.answer(
+            "Я не знаю такой команды, извините", reply_markup=MAIN_KEYBOARD
+        )
     else:
         await order_command(message)
 
