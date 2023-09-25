7803f8fdc7bda47ecef18dca91a7048641dc97d5
unknown-exception <yuri.kropochev@outlook.com>
2023-09-25 17:30:57 +0200

    add content

--- main.py
+++ main.py
@@ -40,7 +40,7 @@ async def check_button(call: types.CallbackQuery):
     tour = call.data.split("_")[2]
     tmp_order[user_id] = tour
     await call.message.answer(f"""Вы хотите записаться на [{tours.get_tour_by_id(tour).head}]
-пожалуйста нажмите /order и напишите ФИО, количество человек/из них детей, контактная информация.""")
+пожалуйста напишите ФИО, количество человек/из них детей, контактная информация.""")
     await call.answer()
 
 @dp.message_handler(commands=["order"])
@@ -48,10 +48,12 @@ async def order_command(message: types.Message):
     user_id = message.chat.id
     tour = tmp_order[user_id]
     tour_name = tours.get_tour_by_id(tour).head
+    del tmp_order[user_id]
     reply = f"""Вы записались на поездку [{tour_name}]!
     
-    Тебе приходит сообщение/письмо 'пользователь {message.from_user.id} {message.from_user.url} {message.from_user.username} записался на {tour_name}, [{message.text.replace("/order")}]
+    Тебе приходит сообщение/письмо 'пользователь {message.from_user.id} {message.from_user.url} {message.from_user.username} записался на {tour_name}, [{message.text.replace("/order","")}]
     """
+    
     await message.reply(reply, reply_markup=MAIN_KEYBOARD)
 
 
@@ -92,9 +94,10 @@ async def tours_list(message: types.Message):
 
 @dp.message_handler()
 async def echo123(message: types.Message):
-    print(message.chat.id)
-    #message.
-    await message.answer("Я не знаю такой команды, извините", reply_markup=MAIN_KEYBOARD)
+    if not tmp_order[message.chat.id]:
+        await message.answer("Я не знаю такой команды, извините", reply_markup=MAIN_KEYBOARD)
+    else:
+        await order_command(message)
 
 
 if __name__ == "__main__":
